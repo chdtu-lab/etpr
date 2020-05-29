218b8e7d5a9d43476d71e341db411ef2de48cca7
Andriy Orehov <andriyorehov@gmail.com>
2020-05-29 12:56:38 +0300

    dont use titled matrix #1

--- src/ETPR/Task3.js
+++ src/ETPR/Task3.js
@@ -1,22 +1,24 @@
 import React, {useEffect, useState} from "react";
 import {useDebouncedCallback} from "use-debounce";
+
 import TextField from "@material-ui/core/TextField";
 import {makeStyles} from "@material-ui/core/styles";
+import Select from '@material-ui/core/Select';
+import FormControl from '@material-ui/core/FormControl';
+import MenuItem from '@material-ui/core/MenuItem';
+import InputLabel from '@material-ui/core/InputLabel';
+
 import fill from 'lodash/fill';
 import lt from 'lodash/lt';
 import lte from 'lodash/lte';
 import gte from 'lodash/gte';
 import gt from 'lodash/gt';
 import eq from 'lodash/eq';
-import Select from '@material-ui/core/Select';
-import FormControl from '@material-ui/core/FormControl';
-import MenuItem from '@material-ui/core/MenuItem';
-import InputLabel from '@material-ui/core/InputLabel';
+
 import Graph from "react-graph-vis";
 import {product} from "iter-tools/es2015";
 import 'katex/dist/katex.min.css';
 import TeX from '@matejmazur/react-katex';
-import {bool} from "prop-types";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -25,7 +27,7 @@ const useStyles = makeStyles(theme => ({
       width: 200,
     },
   },
-  
+
   formControl: {
     margin: theme.spacing(1),
     minWidth: 120,
@@ -49,7 +51,7 @@ function Task3() {
     nodes: [],
     edges: []
   };
-  
+
   const classes = useStyles();
   const [array, setArray] = useState([0, 1, 2, 3, 4]);
   const [graph, setGraph] = useState(initialGraph);
@@ -61,30 +63,31 @@ function Task3() {
   const [comparator, setComparator] = React.useState('eq');
   const [debouncedCallback1] = useDebouncedCallback(value => {
     setArray(value.split(',').map(Number));
-  },1000);
+  }, 1000);
 
   const not = (bool) => !bool;
   const is = (bool) => bool;
+  const clone = (items) => items.map(item => Array.isArray(item) ? clone(item) : item);
 
   useEffect(() => {
     setBinaryRelation(createBinaryRelation(array, is, comparator, false));
     setAdditionBinaryRelation(createBinaryRelation(array, not, comparator, false));
     setReverseBinaryRelation(createBinaryRelation(array, is, comparator, true));
     setDualBinaryRelation(createBinaryRelation(array, not, comparator, true));
-    const zeroArray = fill(Array(array.length + 1), 0);
+    const zeroArray = fill(Array(array.length), 0);
     const zeroMatrix = clone(zeroArray.map(() => zeroArray));
     const filledMatrix = fillMatrix(zeroMatrix, comparator);
-    const matrixWithTitle = addTitleToMatrix(filledMatrix, array)
-    setMatrix(matrixWithTitle);
-    const graph = generateGraph(matrixWithTitle, array);
+    setMatrix(filledMatrix);
+    const graph = generateGraph(filledMatrix, array);
     setGraph(graph);
   }, [comparator, array]);
-  
+
+
   const createBinaryRelation = (array, predicate, comparator, isReverse) => {
     const bR = [];
     const cartesianProduct = Array.from(product(array, array));
     for (let i = 0; i < cartesianProduct.length; i++) {
-      if (predicate(comparators[comparator](cartesianProduct[i][0],cartesianProduct[i][1]))) {
+      if (predicate(comparators[comparator](cartesianProduct[i][0], cartesianProduct[i][1]))) {
         if (isReverse) {
           bR.push(cartesianProduct[i].reverse());
         } else {
@@ -94,9 +97,7 @@ function Task3() {
     }
     return bR;
   }
-  
-  const clone = (items) => items.map(item => Array.isArray(item) ? clone(item) : item);
-  
+
   const fillMatrix = (zeroMatrix, comparator) => {
     for (let i = 0; i < zeroMatrix.length; i++) {
       for (let j = 0; j < zeroMatrix[i].length; j++) {
@@ -107,7 +108,7 @@ function Task3() {
     }
     return zeroMatrix;
   }
-  
+
   const addTitleToMatrix = (matrix, titles) => {
     for (let i = 0; i < matrix.length; i++) {
       for (let j = 0; j < matrix[i].length; j++) {
@@ -124,23 +125,25 @@ function Task3() {
     }
     return matrix;
   }
-  
+
   const generateGraph = (matrix, namedArray) => {
     const graph = {};
-    graph['nodes'] = namedArray.map(i => { return {id: i, label: `${i}`}});
+    graph['nodes'] = namedArray.map(i => {
+      return {id: i, label: `${i}`}
+    });
     graph['edges'] = [];
-    for (let i = 1; i < matrix.length; i++) {
-      for (let j = 1; j < matrix[i].length; j++) {
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix[i].length; j++) {
         if (matrix[i][j] === 1) {
-          graph['edges'].push({ from: i-1, to: j-1 })
+          graph['edges'].push({from: i, to: j})
         }
       }
     }
     return graph;
   }
-  
+
   const handleChange = event => setComparator(event.target.value);
-  
+
   return (
     <>
       <p className="m-3">1.3</p>
@@ -180,20 +183,26 @@ function Task3() {
           </MenuItem>
         </Select>
       </FormControl>
-      <p className="m-3">1.2) Бінарне відношення:  {binaryRelation.map(a => a.join(', ')).map(a => `(${a})`).join(', ')}</p>
-      <p className="m-3">1.5) Доповнення до бінарного відношення:  {additionBinaryRelation.map(a => a.join(', ')).map(a => `(${a})`).join(', ')}</p>
-      <p className="m-3">1.7) Обернене бінарне відношення:  {reverseBinaryRelation.map(a => a.join(', ')).map(a => `(${a})`).join(', ')}</p>
-      <p className="m-3">1.8) Двоїсте бінарне відношення:  {dualBinaryRelation.map(a => a.join(', ')).map(a => `(${a})`).join(', ')}</p>
+      <p className="m-3">1.2) Бінарне
+        відношення: {binaryRelation.map(a => a.join(', ')).map(a => `(${a})`).join(', ')}</p>
+      <p className="m-3">1.5) Доповнення до бінарного
+        відношення: {additionBinaryRelation.map(a => a.join(', ')).map(a => `(${a})`).join(', ')}</p>
+      <p className="m-3">1.7) Обернене бінарне
+        відношення: {reverseBinaryRelation.map(a => a.join(', ')).map(a => `(${a})`).join(', ')}</p>
+      <p className="m-3">1.8) Двоїсте бінарне
+        відношення: {dualBinaryRelation.map(a => a.join(', ')).map(a => `(${a})`).join(', ')}</p>
       <p className="m-3">1.3) Бінарне відношення у матричному вигляді:</p>
-      <div className="m-3">
+      <table id='students'>
+        <tbody>
         {matrix.map((row, i) => (
-          <div key={i}>
+          <tr key={i}>
             {row.map((col, j) => (
-              <span key={j}>{col} {" "}</span>
+              <td key={j} className="border-solid border-2 border-gray-600">{col}</td>
             ))}
-          </div>
+          </tr>
         ))}
-      </div>
+        </tbody>
+      </table>
       <p className="m-3">Бінарне відношення у вигляді орієнтованого графа:</p>
       <div className="m-3">
         <Graph
